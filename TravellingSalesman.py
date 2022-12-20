d472535ceaf7e646458fb7051090886892789c15
Searcherr <alfafemida@gmail.com>
2022-12-20 21:50:37 +0100

    Add TravellingSalesman.py

--- TravellingSalesman.py
+++ TravellingSalesman.py
@@ -1,17 +1,23 @@
 import sys
 import math
+import numpy as np
 
 # Auto-generated code below aims at helping you parse
 # the standard input according to the problem statement.
 
-n = int(input())  # This variables stores how many nodes are given
+n = int(input('Enter the number of nodes: '))  # This variables stores how many nodes are given
+nodes_array = np.zeros(shape=(n, 2))
 for i in range(n):
     # x: The x coordinate of the given node
     # y: The y coordinate of the given node
     x, y = [int(j) for j in input().split()]
+    nodes_array[i] = [x, y]
+
 
 # Write an action using print
 # To debug: print("Debug messages...", file=sys.stderr, flush=True)
 
 # You have to output a valid path
-print("0 2 1 3")
+print('x = ', x)
+print('y = ', y)
+print(nodes_array)
