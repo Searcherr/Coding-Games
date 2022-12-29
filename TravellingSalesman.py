cdc7fe2174691fbcdbf0992266caa12d44ee9766
Searcherr <alfafemida@gmail.com>
2022-12-29 19:44:53 +0100

    Add function creates a graph

--- TravellingSalesman.py
+++ TravellingSalesman.py
@@ -1,25 +1,19 @@
-import sys
 import math
 import numpy as np
 from itertools import permutations
+from sys import maxsize
 
 # Auto-generated code below aims at helping you parse
 # the standard input according to the problem statement.
 
-n = int(input('Enter the number of nodes: '))  # This variables stores how many nodes are given
-nodes_array = np.zeros(shape=(n, 2))
-for i in range(n):
-    # x: The x coordinate of the given node
-    # y: The y coordinate of the given node
-    x, y = [int(j) for j in input().split()]
-    nodes_array[i] = [x, y]
+
 
 
 # Write an action using print
 # To debug: print("Debug messages...", file=sys.stderr, flush=True)
 
 # You have to output a valid path
-print(nodes_array)
+
 
 
 def calculate_distance(nodes_list):
@@ -36,7 +30,6 @@ def create_graph(nodes_list):
     graph = np.zeros(shape=(n, n))
     for i in range(n):
         for j in range(n):
-            #print(np.array([nodes_list[i], nodes_list[j]]))
             graph[i, j] = calculate_distance(np.array([nodes_list[i], nodes_list[j]]))
     return graph
 
@@ -50,6 +43,46 @@ def number_of_roads(number_of_nodes):
     return math.factorial(number_of_nodes - 1)
 
 
+def travellingSalesmanProblem(graph, root_node_number):
+    # making a list of nodes' indexes apart from root node
+    vertex = []
+    for i in range(n):
+        if i != root_node_number:
+            vertex.append(i)
+
+    # storing min distance
+    min_path = maxsize
+    # storing brute force
+    next_permutation = permutations(vertex)
+    for i in next_permutation:
+
+        # store current Path weight(cost)
+        current_pathweight = 0
+
+        # compute current path weight
+        k = root_node_number
+        for j in i:
+            current_pathweight += graph[k][j]
+
+            k = j
+        current_pathweight += graph[k][root_node_number]
+
+        # update minimum
+        min_path = min(min_path, current_pathweight)
+
+    return min_path
+
+
+n = int(input('Enter the number of nodes: '))  # This variables stores how many nodes are given
+nodes_array = np.zeros(shape=(n, 2))
+for i in range(n):
+    # x: The x coordinate of the given node
+    # y: The y coordinate of the given node
+    x, y = [int(j) for j in input().split()]
+    nodes_array[i] = [x, y]
+
+print(nodes_array)
+
 nodes_array_distance = calculate_distance(nodes_array)
 record_distance = nodes_array_distance
 
@@ -60,7 +93,18 @@ print(temp)
 for _ in range(number_of_roads(n)):
     swap_nodes_in_array()
 """
+users_graph = create_graph(nodes_array)
 
-print(f"Your graph: \n {create_graph(nodes_array)}")
+print(f"Your graph: \n {users_graph}")
 print(f"Distance = {nodes_array_distance}")
 
+starting_node_index = 0
+resulr_distance = travellingSalesmanProblem(users_graph, starting_node_index)
+print(f"TSP distance = {resulr_distance}")
+
+test_graph = np.array([[1, 1],
+                       [2, 2],
+                       [3, 3],
+                       [1, 1]])
+test_result = calculate_distance(test_graph)
+print(f"Test graph distance = {test_result}")
